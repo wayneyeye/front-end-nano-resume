5db28e0c8128c17e17bc093a77ef6894464d868d
yewenhe0904 <yewenhe0904@gmail.com>
2016-12-30 21:27:04 -0600

    setup js skeleton (4 objs)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,21 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var bio, education, work, projects;
+// Bio Part
+bio = {
+
+ };
+// Education Part
+education = {
+
+};
+// Work Part
+work = {
+
+ };
+// Projects Part
+projects = {
+
+};
