78de122d2823ca8913fa98bad7004ef5f407ad20
yewenhe0904 <yewenhe0904@gmail.com>
2016-12-30 21:43:38 -0600

    filled bio info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,12 +1,28 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-
+ 
  var bio, education, work, projects;
 // Bio Part
 bio = {
-
+	name : "Wayne Ye",
+	role : "Front-end Developer",
+	contacts : {
+		mobile: "222-333-4444",
+		emaile: "wayneye@udacity.com",
+		github: "wayneye.github.io",
+		location: "Dallas, TX"
+	},
+	welcomeMessage : "Welcome to my resume site!",
+	skills : ["Javascript", "HTML", "CSS", "Awesomeness"],
+	biopic : "images/fry.jpg",
+	display : function() {
+		// display bio function goes from here!
+		console.log(bio);
+	}
  };
+// bio.display();
+
 // Education Part
 education = {
 
