99e3708659056d8c85a2e57922cd64fe077385b5
yewenhe0904 <yewenhe0904@gmail.com>
2016-12-31 20:20:14 -0600

    fill education part

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -24,8 +24,38 @@ bio = {
 
 // Education Part
 education = {
-
+	schools : [
+		{
+			name : "Northwestern University",
+			location : "Evanston, IL",
+			degree : "Master of Science",
+			majors : ["Engineering"],
+			dates : "2013 - 2015",
+			url : "#Northwestern"
+		},
+		{
+			name : "University of Texas at Dallas",
+			location : "Dallas, TX",
+			degree : "Master of Science",
+			majors : ["Management Science"],
+			dates : "2017 -",
+			url : "#UTDallas"
+		}
+	],
+	onlineCourses : [
+		{
+			title : "Data Science",
+			school : "Coursera",
+			dates : "2015 - 2016",
+			url : "#Coursera"
+		}
+	],
+	display : function() {
+		// display bio function goes from here!
+		console.log(education);
+	}
 };
+// education.display();
 // Work Part
 work = {
 
